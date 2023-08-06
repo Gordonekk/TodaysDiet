9de9a1c8dbeeb622c399539dcf9738551749c3e0
Gordonekk <hwut2rxt+github@posteo.org>
2023-08-07 00:30:01 +0200

    Added models

--- app/website/models.py
+++ app/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    TYPES = [
+            ("HQP", "High-quality protein source"),
+            ("HQPF", "High-quality protein with fats"),
+            ("CC", "Complex carbohydrates"),
+            ("F", "Fiber"),
+    ]
+    name = models.CharField(max_length=60)
+    kind = models.CharField(max_length=4, choices=TYPES)
+    
+    def __str__(self):
+        return self.name
